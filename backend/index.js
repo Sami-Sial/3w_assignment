9b239a28c3d1789c06a5e97c9239b045d2efd41c
Sami-Sial <samisial1555@gmail.com>
2025-01-21 19:28:11 +0500

    add change

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,10 @@ const cors = require("cors");
 
 // Data Parsing
 app.use((req, res, next) => {
-    res.set({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-    });
-
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
     next();
 });
 app.use(express.urlencoded({ extended: true }));
