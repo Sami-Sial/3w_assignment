303156d0514b755f31d2ab57ef28234fd8caea89
Sami-Sial <samisial1555@gmail.com>
2025-01-22 07:13:14 +0500

    add change

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const app = express();
-const cors = require("cors");
+const cors = require("cors")
 
 
 // Data Parsing
